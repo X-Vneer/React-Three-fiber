5a20fdcb5421f9079c0152fe45f331490f712c64
X_V.neer <xv.neer@gmail.com>
2022-12-19 18:50:09 +0200

    updated react three drei

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Three from "./components/Three";
+import { Loader } from "@react-three/drei";
 
 function App() {
   return (
     <>
       <Canvas id="main-canvas" shadows={true} camera={{ position: [0, 0, 5] }}>
-        <Suspense>
+        <Suspense fallback={null}>
           <Three />
         </Suspense>
       </Canvas>
+      <Loader />
     </>
   );
 }
--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -5,11 +5,12 @@ import {
   Environment,
   Float,
   OrbitControls,
-  softShadows,
+  PresentationControls,
+  Grid,
 } from "@react-three/drei";
 import { LayerMaterial, Noise, Color, Gradient } from "lamina";
+import { useControls } from "leva";
 
-softShadows();
 function Striplight(props) {
   return (
     <mesh {...props}>
@@ -19,18 +20,37 @@ function Striplight(props) {
   );
 }
 const Three = () => {
+  const { gridSize, ...gridConfig } = useControls({
+    gridSize: [10.5, 10.5],
+    cellSize: { value: 0.6, min: 0, max: 10, step: 0.1 },
+    cellThickness: { value: 1, min: 0, max: 5, step: 0.1 },
+    cellColor: "#6f6f6f",
+    sectionSize: { value: 3.3, min: 0, max: 10, step: 0.1 },
+    sectionThickness: { value: 1.5, min: 0, max: 5, step: 0.1 },
+    sectionColor: "#7354ff",
+    fadeDistance: { value: 25, min: 0, max: 100, step: 1 },
+    fadeStrength: { value: 1, min: 0, max: 1, step: 0.1 },
+    followCamera: false,
+    infiniteGrid: true,
+  });
   return (
     <>
       <pointLight position={[10, 10, 5]} />
       <pointLight position={[-10, -10, -5]} />
       <OrbitControls
         autoRotate
+        rotateSpeed={0.4}
+        autoRotateSpeed={0.5}
         minPolarAngle={Math.PI / 2.8}
         maxPolarAngle={Math.PI / 1.8}
         minDistance={3}
         maxDistance={10}
       />
       <group position={[0, -1.5, 0]}>
+        {/* <PresentationControls
+          config={{ mass: 2, tension: 500 }}
+          snap={{ mass: 4, tension: 1500 }}
+        > */}
         <Float
           position={[0, 2.15, 0]}
           speed={2}
@@ -46,7 +66,9 @@ const Three = () => {
             />
           </mesh>
         </Float>
-        <ContactShadows scale={10} blur={3} opacity={0.25} far={15} />
+        {/* </PresentationControls> */}
+        <ContactShadows scale={10} blur={3} opacity={0.25} far={30} />
+        <Grid args={gridSize} {...gridConfig} />
       </group>
 
       <ambientLight args={["#ffffff", 0.4]} />
